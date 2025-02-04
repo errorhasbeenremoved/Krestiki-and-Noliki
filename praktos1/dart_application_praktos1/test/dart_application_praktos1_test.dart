30cbe1c7eb8e1bccb9161ccfa0a7811400caf8b3
errorhasbeenremoved <veraglvch@gmail.com>
2025-02-04 21:52:15 +0300

    Delete praktos1/dart_application_praktos1 directory

--- praktos1/dart_application_praktos1/test/dart_application_praktos1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_application_praktos1/dart_application_praktos1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
